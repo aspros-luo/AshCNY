bc48f18fe7e330d78fd3c481edb0f6591d7caa55
Aspros <aspros_l@outlook.com>
2016-06-07 14:39:23 +0800

    fix bug

--- app/src/main/java/ash/aiqinhaigou/com/ashcny/index/presenter/GoodsPresenter.java
+++ app/src/main/java/ash/aiqinhaigou/com/ashcny/index/presenter/GoodsPresenter.java
@@ -1,10 +1,6 @@
 package ash.aiqinhaigou.com.ashcny.index.presenter;
 
-import java.util.List;
-
-import ash.aiqinhaigou.com.ashcny.model.Subject;
 import ash.aiqinhaigou.com.ashcny.presenter.SubscriberOnNextListener;
-import rx.Subscriber;
 
 /**
  * Created by Aspros on 16/6/1.
