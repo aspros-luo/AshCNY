3cbb44cbfd21c17dc4fad567c3f534fc76e9b9fa
Aspros <aspros_l@outlook.com>
2016-06-07 14:35:09 +0800

    增加 请求网络电影详情数据

--- app/src/main/java/ash/aiqinhaigou/com/ashcny/index/model/GoodsModel.java
+++ app/src/main/java/ash/aiqinhaigou/com/ashcny/index/model/GoodsModel.java
@@ -1,14 +1,20 @@
 package ash.aiqinhaigou.com.ashcny.index.model;
 
-import java.util.List;
-
-import ash.aiqinhaigou.com.ashcny.model.Subject;
 import ash.aiqinhaigou.com.ashcny.presenter.SubscriberOnNextListener;
-import rx.Subscriber;
 
 /**
  * Created by Aspros on 16/5/31.
  */
 public interface GoodsModel {
+    //加载列表 方法
     void loadGoods(String url, int type, int pageIndex, GoodsModelImpl.OnLoadGoodsListListener listListener,SubscriberOnNextListener subscriberOnNextListener);
+
+    /*
+    方法:加载详细信息
+    参数:
+    id:商品/电影的id
+    listListener:用于监听成功或失败事件
+    subscriberOnNextListener:订阅者
+     */
+    void loadGoodsDetail(String id,GoodsModelImpl.OnLoadGoodsListListener listListener, SubscriberOnNextListener subscriberOnNextListener);
 }
