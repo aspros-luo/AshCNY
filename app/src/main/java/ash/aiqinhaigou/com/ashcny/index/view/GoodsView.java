232c28ee17e8ece95c6b678cd0123c17ca266553
Aspros <aspros_l@outlook.com>
2016-06-07 14:40:17 +0800

    增加注视

--- app/src/main/java/ash/aiqinhaigou/com/ashcny/index/view/GoodsView.java
+++ app/src/main/java/ash/aiqinhaigou/com/ashcny/index/view/GoodsView.java
@@ -1,9 +1,7 @@
 package ash.aiqinhaigou.com.ashcny.index.view;
 
 import java.util.List;
-
-import ash.aiqinhaigou.com.ashcny.bean.GoodsBean;
-import ash.aiqinhaigou.com.ashcny.model.Subject;
+import ash.aiqinhaigou.com.ashcny.bean.SubjectsBean;
 
 /**
  * Created by Aspros on 16/5/31.
@@ -12,7 +10,7 @@ public interface GoodsView {
     // before load data, show your progress bar
     void showProgress();
     // loading data
-    void addData(List<Subject> goodsBeanList);
+    void addData(List<SubjectsBean> goodsBeanList);
     // after load data ,we need to hide progress bar
     void hideProgress();
     // if we make a mistake ,we need to show the msg
