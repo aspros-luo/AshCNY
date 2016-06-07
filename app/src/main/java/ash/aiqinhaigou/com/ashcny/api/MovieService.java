df22a22c36e2d9f619dc05944dbf989970aff1aa
Aspros <aspros_l@outlook.com>
2016-06-07 10:54:46 +0800

    添加注释， 添加相关方法获取不同数据

--- app/src/main/java/ash/aiqinhaigou/com/ashcny/api/MovieService.java
+++ app/src/main/java/ash/aiqinhaigou/com/ashcny/api/MovieService.java
@@ -3,10 +3,12 @@ package ash.aiqinhaigou.com.ashcny.api;
 
 import java.util.List;
 
-import ash.aiqinhaigou.com.ashcny.bean.GoodsBean;
+import ash.aiqinhaigou.com.ashcny.bean.MovieBean;
+import ash.aiqinhaigou.com.ashcny.bean.SubjectsBean;
 import ash.aiqinhaigou.com.ashcny.model.HttpResult;
 import ash.aiqinhaigou.com.ashcny.model.Subject;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -27,6 +29,26 @@ public interface MovieService {
     @GET("top250")
     Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
 
+    /**
+     * 用于查询Top250 使用@Query 拼接参数
+     * @param start 起始位置
+     * @param count 长度
+     * @return
+     */
     @GET("top250")
-    Observable<HttpResult<List<GoodsBean>>> getGoodsBean(@Query("start") int start, @Query("count") int count);
+    Observable<HttpResult<List<SubjectsBean>>> getGoodsBean(@Query("start") int start, @Query("count") int count);
+
+    @GET("in_theaters")
+    Observable<HttpResult<List<SubjectsBean>>> getInTheaters(@Query("city") String city,@Query("start") int start,@Query("count") int count);
+
+    @GET("coming_soon")
+    Observable<HttpResult<List<SubjectsBean>>> getComeSoon(@Query("start") int start,@Query("count") int count);
+
+    /**
+     * 通过id查询电影详情，使用@Path 替代｛id｝的参数
+     * @param movieId 电影Id
+     * @return
+     */
+    @GET("subject/{id}")
+    Observable<MovieBean> getMovieDetail(@Path("id") String movieId);
 }
