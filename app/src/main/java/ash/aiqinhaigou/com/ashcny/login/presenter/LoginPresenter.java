a08c33c243c160d5bb27dde7b46cd31af612fbfe
Aspros <aspros_l@outlook.com>
2016-06-12 16:57:40 +0800

    定义Presenter 登录接口

---
+++ app/src/main/java/ash/aiqinhaigou/com/ashcny/login/presenter/LoginPresenter.java
@@ -0,0 +1,8 @@
+package ash.aiqinhaigou.com.ashcny.login.presenter;
+
+/**
+ * Created by Aspros on 16/6/7.
+ */
+public interface LoginPresenter {
+    void loginSys(String name, String password);
+}
